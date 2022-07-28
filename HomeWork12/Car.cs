21a77248aa2b548f19ea1d3228d403caa7e2afc8
Maksym Semenov <mestcomua@gmail.com>
2022-07-28 20:20:33 +0300

    Добавьте файлы проекта.

---
+++ HomeWork12/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+
+namespace HomeWork12
+{
+    [Serializable]
+    [DataContract]
+    public class Car
+    {
+        private string name;
+        private string description;
+        private int price;
+        [XmlAttribute]
+        [DataMember]
+        public string Name { get { return name; } set { name = value; } }
+        [XmlAttribute]
+        [DataMember]
+
+        public string Description { get { return description; } set { description = value; } }
+        [XmlAttribute]
+        [DataMember]
+
+        public int Price { get { return price; } set { price = value; } }
+        public Car()
+        {
+
+        }
+        public Car(string name, string description, int price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+        }
+
+    }
+}
---
+++ HomeWork12/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Xml.Serialization;
+
+namespace HomeWork12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] car = new Car[3];
+            car[0] = new Car("Model X","white",50000);
+            car[1] = new Car("Model L", "grey", 60000);
+            car[2] = new Car("Model L", "red", 70000);
+            //BIN
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("Car.bin",FileMode.Create,FileAccess.Write);
+
+            formatter.Serialize(stream,car);
+            stream.Close();
+
+            stream = new FileStream("Car.bin", FileMode.Open, FileAccess.Read);
+            Car[] carbin = (Car[])formatter.Deserialize(stream);
+            stream.Close();
+
+            foreach (var item in carbin)
+            {
+                Console.WriteLine($"Bi {item.Name}, {item.Description}, {item.Price} ");
+            }
+            //XML
+            XmlSerializer serializer = new XmlSerializer(typeof(Car[]));
+            FileStream fs = new FileStream("car.xml", FileMode.Create);
+            
+                serializer.Serialize(fs, car);
+            fs.Close();
+            fs = new FileStream("car.xml", FileMode.Open);
+            
+            Car[] carxml = serializer.Deserialize(fs) as Car[];
+            fs.Close();
+
+            foreach (var item in carxml)
+            {
+                Console.WriteLine($"XML {item.Name}, {item.Description}, {item.Price} ");
+            }
+            //JSON
+            Stream file = new FileStream("car.json", FileMode.Create);
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Car[]));
+
+            ser.WriteObject(file, car);
+
+            file.Position = 0;
+
+            Car[] carjson = (Car[])ser.ReadObject(file);
+
+
+            foreach (var item in carjson)
+            {
+                Console.WriteLine($"JSON {item.Name}, {item.Description}, {item.Price} ");
+            }
+        }
+    }
+}
